4a75f87de8908f11a5a01214110662cf4b8ba7b8
jamesandfamily <ja.me.sand.fa.m.ily.is@googlemail.com>
2025-11-12 16:11:56 +0530

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  basename: "/real-estate-app", 
   future: {
     unstable_optimizeDeps: true,
   },
